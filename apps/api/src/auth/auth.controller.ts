d2c35b4d301e5f8d50fe3ca0fb3a8e212ff9d07d
alex-sdn <alexsdnwcz@gmail.com>
2023-10-16 23:03:45 +0200

    rm comment

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Post, Query, Res } from "@nestjs/common";
+import { Controller, Get, Query, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
-	@Get() // post ?
-	async handleOAuthCallback(@Query('code') code: string, @Res() response) {
+	@Get()
+	handleOAuthCallback(@Query('code') code: string, @Res() response) {
 		return this.authService.handleOAuthCallback(code, response);
 	}
 }
