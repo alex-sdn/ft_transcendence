8b2efb616c4a91ed643181c241b548b87e9745b1
alex-sdn <alexsdnwcz@gmail.com>
2023-10-18 00:14:38 +0200

    auth: getUserInfo() broken

--- monorepo/apps/api/src/auth/auth.module.ts
+++ monorepo/apps/api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+	imports: [HttpModule],
 	controllers: [AuthController],
 	providers: [AuthService],
 })
--- monorepo/apps/api/src/auth/auth.service.ts
+++ monorepo/apps/api/src/auth/auth.service.ts
@@ -1,8 +1,11 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { map } from "rxjs";
 
 @Injectable()
 export class AuthService {
+	constructor(private httpService: HttpService) {}
 
 	async handleOAuthCallback(code: string, response) {
 		const tokenEndpoint = 'https://api.intra.42.fr/oauth/token';
@@ -24,6 +27,8 @@ export class AuthService {
 
 			// GET USER INFO FROM 42API HERE
 			console.log('ACCESS:', accessToken, "\nREFRESH:", refreshToken);
+			const userInfo = await this.getUserInfo(accessToken);
+			console.log(userInfo);
 
 			response.redirect('/success');
 		} catch (error) {
@@ -31,4 +36,44 @@ export class AuthService {
 			response.redirect('/error');
 		}
 	}
+
+	async getUserInfo(accessToken: string) {
+		const userEndpoint = "https://api.intra.42.fr/v2/me";
+
+		try {
+			const response = this.httpService.get(
+				userEndpoint, {
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+				}
+			);
+
+			return response;
+			// ).pipe(
+			// 	map((response: AxiosResponse) => {
+			// 		console.log('TEST ${accesToken}');
+			// 		const fullInfo = response.data;
+
+			// 		console.log(fullInfo);
+
+			// 		const userInfo: UserInfo = {
+			// 			userId: fullInfo.id,
+			// 			login: fullInfo.login,
+			// 		};
+					
+			// 		return userInfo;
+			// 	})
+			// );
+		} catch (error) {
+			// handle error here
+			throw new Error('Failed to fetch user info');
+		}
+	}
+}
+
+//move elsewhere ?
+interface UserInfo {
+	userId: string;
+	login: string;
 }
