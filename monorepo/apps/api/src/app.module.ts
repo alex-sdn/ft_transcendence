0dcead31367302452cce842c44a89f21dc57bc27
alex-sdn <alexsdnwcz@gmail.com>
2023-10-17 14:09:25 +0200

    pb watch mode dans docker

---
+++ monorepo/apps/api/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+	ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '../..', 'front', 'dist'),
+	}),
+	AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
