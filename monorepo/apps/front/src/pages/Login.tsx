276e831bcf656c9060665a1e7801b53186d3723a
Solene <aguesse.solene2@neuf.fr>
2023-10-23 17:10:09 +0200

    username form

--- monorepo/apps/front/src/pages/Login.tsx
+++ monorepo/apps/front/src/pages/Login.tsx
@@ -11,8 +11,8 @@ const Login: React.FC = () => {
         .then(response => response.text()) // Ensure the response is treated as text
         .then(data => {
             // 'data' will be the string returned by the backend
-            if (data === 'notfound') 
-                return navigate('/game');
+            if (data === 'notfound')
+                return navigate('/username');
             console.log(data);
             // You can then set it in your component's state or use it as needed.
         })
@@ -21,7 +21,9 @@ const Login: React.FC = () => {
         });
 
     return (
-        <h2>Login</h2>
+        <h2 className='login'>
+            <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1b7f717c58b58406ad4b2abe9145475069d66ace504146041932a899c47ff960&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code">Login with 42</a>
+        </h2>
     );
 }
 
