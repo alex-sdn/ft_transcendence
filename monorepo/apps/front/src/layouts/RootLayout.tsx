2ec042c600cc860a11a7d9726b6d1db4c4ee31a5
Solene <aguesse.solene2@neuf.fr>
2023-10-23 17:10:09 +0200

    username form

--- monorepo/apps/front/src/layouts/RootLayout.tsx
+++ monorepo/apps/front/src/layouts/RootLayout.tsx
@@ -11,7 +11,6 @@ const RootLayout: React.FC = () => {
           <NavLink to="game">Game</NavLink>
           <NavLink to="chat">Chat</NavLink>
           <NavLink to="profile">Profile</NavLink>
-          <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1b7f717c58b58406ad4b2abe9145475069d66ace504146041932a899c47ff960&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code">Login</a>
         </nav>
       </header>
       <main>
