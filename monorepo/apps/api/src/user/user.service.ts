90c4e732f6a3c5cb976f5251952c9cc15b18d391
Alexandre SADANOWICZ <asadanow@made-f0Br7s12.clusters.42paris.fr>
2023-10-23 16:24:20 +0200

    add editNickname endpoint

--- monorepo/apps/api/src/user/user.service.ts
+++ monorepo/apps/api/src/user/user.service.ts
@@ -1,10 +1,13 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-
+import { ForbiddenException, HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { AuthService } from "../auth/auth.service";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class UserService {
-	constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService,
+		private authService: AuthService) {}
 
 	async getUser(nickname: string) {
 		const user = await this.prisma.user.findUnique({
@@ -16,5 +19,31 @@ export class UserService {
 			throw new HttpException('USER_NOT_FOUND', HttpStatus.NOT_FOUND);
 
 		return user;
+	}
+
+	async editNickname(user, nickname: string) {
+		// check if nickname empty or invalid chars etc
+		if (!nickname)
+			throw new HttpException('MISSING_CREDENTIALS', HttpStatus.BAD_REQUEST);
+		try {
+			// update user
+			const updatedUser = await this.prisma.user.update({
+				where: {
+					id: user.id,
+				},
+				data: {
+					nickname
+				}
+			});
+			// returns new JWT (necessaire?)
+			return this.authService.signToken(updatedUser.id, updatedUser.nickname, false);
+		} catch(error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException('Credentials taken');
+				}
+			}
+			throw error;
+		};
 	}
 }
