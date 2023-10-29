022735db1aaed150e15b28c002af3bfb7e030742
smk <sacha.mkara@gmail.com>
2023-10-29 21:01:16 +0100

    just adding some new tools (swagger, dbdiagram, makefile...)

--- monorepo/apps/api/src/main.ts
+++ monorepo/apps/api/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  //SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('ft_transcendence')
+    .setDescription('pong game')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'api')
+    .addTag('pong')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
