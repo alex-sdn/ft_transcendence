78e310777bae74d19ea7597c73d936694ccd9bbb
alex-sdn <alexsdnwcz@gmail.com>
2023-10-31 22:13:52 +0100

    [chat] add JOIN channel (wip)

--- monorepo/apps/api/src/chat/chat.service.ts
+++ monorepo/apps/api/src/chat/chat.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
-// @Injectable
+@Injectable()
 export class ChatService {
+	constructor(private prisma: PrismaService) {}
 
+	
 }
--- monorepo/apps/api/src/chat/gateway/chat.gateway.ts
+++ monorepo/apps/api/src/chat/gateway/chat.gateway.ts
@@ -2,18 +2,23 @@ import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect,
 import { User } from "@prisma/client";
 import { Server, Socket } from 'socket.io';
 import { AuthService } from "src/auth/auth.service";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @WebSocketGateway({namespace: 'chat'})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	constructor(private authService: AuthService) {}
+	constructor(
+		private authService: AuthService,
+		private prisma: PrismaService) {}
 
 	@WebSocketServer()
 	server: Server;
 
+	// < user.id, Socket >
+	private userToSocket = new Map<number, Socket>();
 	// < client.id, User >  (replace user with userId for updates?)
-	private clients = new Map<string, User>();
+	private idToUser = new Map<string, User>();
 
-	// Add user to connected clients map if jwt OK
+	// Add user to maps if jwt OK, disconnect if not
 	async handleConnection(client: any, ...args: any[]) {
 		console.log("New chat WS connection attempted ("+client.id+")");
 
@@ -24,23 +29,107 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		}
 		else {
 			console.log('Connection accepted for', user.nickname);
-			this.clients.set(client.id, user);
+			// add to maps
+			this.userToSocket.set(user.id, client);
+			this.idToUser.set(client.id, user);
 		}
 	}
 
 	handleDisconnect(client: any) {
 		console.log(client.id, "disconnected");
-		this.clients.delete(client.id);
+		// rm from maps
+		this.userToSocket.delete(this.idToUser.get(client.id).id);
+		this.idToUser.delete(client.id);
 	}
 
-	@SubscribeMessage('message')
+	@SubscribeMessage('message')  // ==channel
 	handleMessage(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
-		console.log("[msg] ", this.clients.get(client.id).nickname + ":", message);
+		console.log("[msg] ", this.idToUser.get(client.id).nickname + ":", message);
+	}
+
+	@SubscribeMessage('privmsg')  // ==dm
+	async handlePrivMessage(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
+		// console.log("[msg] ", this.idToUser.get(client.id).nickname + ":", message);
+
+		// Get target user
+		const target = await this.prisma.user.findUnique({
+			where: { nickname: message.target }
+		});
+		if (!target) {
+			console.log('target not found');
+			return;//target not found
+		}
+
+		// Get target socket
+		const targetSocket = this.userToSocket.get(target.id);
+		if (!targetSocket) {
+			console.log('target not connected to ws');
+			return;//target not connected
+		}
+		
+		const sender = this.idToUser.get(client.id).nickname;
+		// Send to target AND sender (?)
+		targetSocket.emit('privmsg', {
+			sender: sender,
+			message: message.message
+		});
+		client.emit('privmsg', {
+			sender: sender,
+			message: message.message
+		});
 	}
 
 	@SubscribeMessage('join')
-	handleJoin(@MessageBody() message: any) {
+	async handleJoin(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
 		console.log("join", message);
+
+		const user = this.idToUser.get(client.id);
+
+		const channel = await this.prisma.channel.findUnique({
+			where: {name: message.target},
+			include: {members: true}
+			// + include banned and verify
+		});
+
+		if (!channel) {
+			console.log('channel does not exist, connection failed'); return;
+		}
+
+		if (channel.access === 'private') {
+			// check if invited here!!!!
+			// Send fail message ?
+			console.log('channel private, (not invited)'); return;
+		}
+		else if (channel.access === 'protected' && message.password !== channel.password) {
+			// Send fail message ?
+			console.log('channel protected, incorrect password'); return;
+		}
+
+		// add User to channel users[]
+		await this.prisma.channel.update({
+			where: {id: channel.id},
+			data: {
+				members: {
+					connect: {id: user.id}}
+			}
+		});
+
+		// send message to client if successful
+		client.emit('join', {
+			sender: user.nickname,
+			message: 'has joined the channel'
+		});
+		// send message to all users ?
+		for (var i in channel.members) {
+			const socket = this.userToSocket.get(channel.members[i].id);
+			// if channel member is connected -> send JOIN message
+			if (socket) {
+				socket.emit('join', {
+					sender: user.nickname,
+					message: 'has joined the channel'
+				});
+			}
+		}
 	}
 
 }
