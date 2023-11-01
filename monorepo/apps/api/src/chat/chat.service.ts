b608c1c194bb4e0c7f15c62af261ec824ca96659
alex-sdn <alexsdnwcz@gmail.com>
2023-11-02 00:07:23 +0100

    progres Chat + update DB schema

--- monorepo/apps/api/src/chat/chat.service.ts
+++ monorepo/apps/api/src/chat/chat.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from "@nestjs/common";
+import { Channel, ChatAccess, User } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class ChatService {
 	constructor(private prisma: PrismaService) {}
 
-	
+	async joinChannel(channel, user: User, password: string) {
+		// IF CHANNEL DOESN'T EXIST
+		if (!channel) {
+			throw new Error('channel does not exist, connection failed');
+		}
+		// IF USER IN CHANNEL
+		if (channel.members.find(member => {return member.id === user.id;})) {
+			throw new Error('you are already in this channel');
+		}
+		// IF CHANNEL PRIVATE AND NOT INVITED
+		if (channel.access === 'private') {
+			// check if invited here!!!!
+			throw new Error('channel private, (not invited)');
+		}
+		// IF BANNED FROM CHANNEL
+		else if (channel.bans.includes(user.id)) {
+			throw new Error('you are banned from this channel');
+		}
+		// IF CHANNEL PROTECTED AND WRONG PASSWORD
+		else if (channel.access === 'protected' && password !== channel.password) {
+			throw new Error('channel protected, incorrect password');
+		}
+
+		// add User to channel users[]
+		await this.prisma.channel.update({
+			where: {id: channel.id},
+			data: {
+				members: {
+					connect: {id: user.id}}
+			}
+		});
+	}
+
+	async createChannel(user: User, message) {
+		if (!message.target)
+			throw new Error('no channel name specified');
+
+		const checkTaken = await this.prisma.channel.findUnique({
+			where: {name: message.target}
+		});
+		// IF NAME IS TAKEN
+		if (checkTaken) {
+			throw new Error('channel name already taken');
+		}
+		// IF MISSING PASSWORD
+		if (message.access === 'protected' && !message.password)
+			throw new Error('missing password for protected access');
+		
+		try {
+			// Create channel
+			const channel = await this.prisma.channel.create({
+				data: {
+					name: message.target,
+					access: message.access,
+					password: message.password
+				}
+			});
+			// Add user  (+set as owner! +admin)
+			await this.prisma.channel.update({
+				where: {id: channel.id},
+				data: {
+					members: {
+						connect: {id: user.id}
+					}
+				}
+			});
+		} catch(error) {
+			throw new Error('Failed to create channel');
+		}
+	}
 }
--- monorepo/apps/api/src/chat/gateway/chat.gateway.ts
+++ monorepo/apps/api/src/chat/gateway/chat.gateway.ts
@@ -3,10 +3,12 @@ import { User } from "@prisma/client";
 import { Server, Socket } from 'socket.io';
 import { AuthService } from "src/auth/auth.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { ChatService } from "../chat.service";
 
 @WebSocketGateway({namespace: 'chat'})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	constructor(
+		private chatService: ChatService,
 		private authService: AuthService,
 		private prisma: PrismaService) {}
 
@@ -43,8 +45,42 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	}
 
 	@SubscribeMessage('message')  // ==channel
-	handleMessage(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
+	async handleMessage(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
 		console.log("[msg] ", this.idToUser.get(client.id).nickname + ":", message);
+	
+		const user = this.idToUser.get(client.id);
+
+		const channel = await this.prisma.channel.findUnique({
+			where: {name: message.target},
+			include: {members: true}
+		});
+
+		if (!channel) {
+			//emit fail msg?
+			console.log('target channel does not exist'); return;
+		}
+
+		if (!channel.members.find(member => {return member.id === user.id;})) {
+			//emit fail msg?
+			console.log('you are not in this channel'); return;
+		}
+
+		// VERIFY IF NOT MUTED IN CHANNEL
+
+		//send to all connected members (including sender)
+		for (var i in channel.members) {
+			const socket = this.userToSocket.get(channel.members[i].id);
+			// if channel member is connected -> emit message
+			// VERIFY IF SENDER NOT MUTED BY MEMBER
+			if (socket) {
+				socket.emit('message', {
+					sender: user.nickname,
+					target: message.target,
+					message: message.message
+				});
+			}
+			console.log('looped');
+		}
 	}
 
 	@SubscribeMessage('privmsg')  // ==dm
@@ -56,15 +92,15 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 			where: { nickname: message.target }
 		});
 		if (!target) {
-			console.log('target not found');
-			return;//target not found
+			//emit fail msg?
+			console.log('target not found'); return;
 		}
 
 		// Get target socket
 		const targetSocket = this.userToSocket.get(target.id);
-		if (!targetSocket) {
-			console.log('target not connected to ws');
-			return;//target not connected
+		if (!targetSocket) {  //target not connected
+			//emit fail msg?
+			console.log('target not connected to ws'); return;
 		}
 		
 		const sender = this.idToUser.get(client.id).nickname;
@@ -88,47 +124,51 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		const channel = await this.prisma.channel.findUnique({
 			where: {name: message.target},
 			include: {members: true}
-			// + include banned and verify
 		});
 
-		if (!channel) {
-			console.log('channel does not exist, connection failed'); return;
-		}
-
-		if (channel.access === 'private') {
-			// check if invited here!!!!
-			// Send fail message ?
-			console.log('channel private, (not invited)'); return;
-		}
-		else if (channel.access === 'protected' && message.password !== channel.password) {
-			// Send fail message ?
-			console.log('channel protected, incorrect password'); return;
-		}
-
-		// add User to channel users[]
-		await this.prisma.channel.update({
-			where: {id: channel.id},
-			data: {
-				members: {
-					connect: {id: user.id}}
+		try {
+			await this.chatService.joinChannel(channel, user, message.password);
+			// send message to client if successful
+			client.emit('join', {
+				sender: user.nickname,
+				target: message.target,
+				message: 'has joined the channel'
+			});
+			// send message to all users ?
+			for (var i in channel.members) {
+				const socket = this.userToSocket.get(channel.members[i].id);
+				// if channel member is connected -> send JOIN message
+				if (socket) {
+					socket.emit('join', {
+						sender: user.nickname,
+						target: message.target,
+						message: 'has joined the channel'
+					});
+				}
 			}
-		});
+		} catch(error) {  // maybe verify error type
+			//send error message to socket?
+			console.log(error.message);
+		}
+	}
 
-		// send message to client if successful
-		client.emit('join', {
-			sender: user.nickname,
-			message: 'has joined the channel'
-		});
-		// send message to all users ?
-		for (var i in channel.members) {
-			const socket = this.userToSocket.get(channel.members[i].id);
-			// if channel member is connected -> send JOIN message
-			if (socket) {
-				socket.emit('join', {
-					sender: user.nickname,
-					message: 'has joined the channel'
-				});
-			}
+	@SubscribeMessage('create')
+	async handleCreate(@ConnectedSocket() client: Socket, @MessageBody() message: any) {
+		console.log("create", message);
+
+		const user = this.idToUser.get(client.id);
+	
+		try {
+			await this.chatService.createChannel(user, message);
+
+			client.emit('create', {
+				sender: user.nickname,
+				target: message.target,
+				message: 'has created the channel'
+			});
+		} catch(error) {  // maybe verify error type
+			//send error message to socket?
+			console.log(error.message);
 		}
 	}
 
