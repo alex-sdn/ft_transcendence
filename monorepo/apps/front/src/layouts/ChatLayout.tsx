e516a74381f6305ca03ed1eab91bbc3a5b877f96
Solene <aguesse.solene2@neuf.fr>
2023-11-07 19:11:19 +0100

    join channel error redir

--- monorepo/apps/front/src/layouts/ChatLayout.tsx
+++ monorepo/apps/front/src/layouts/ChatLayout.tsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import Modal from "react-modal";
+import Cookies from "js-cookie";
+import axios from "axios";
+
 import CreateChannel from "../pages/chat/CreateChannel";
 import SocketContext from "../Socket";
-import { Socket } from "socket.io-client";
-import axios from "axios";
 
 const customStyles = {
     content: {
@@ -19,58 +19,112 @@ const customStyles = {
     },
 };
 
+interface channels {
+    name: string;
+    access: string;
+}
+
 const ChatLayout: React.FC = () => {
-    // let channels = ["Chocolat", "Chien", "Chat", "Cafeine", "Cafe"];
-    const [value, setValue] = useState<string>("");
     const socket = useContext(SocketContext);
-
     const jwtToken = Cookies.get('jwt-token');
-    const [allChannels, setAllChannels] = useState([]);
 
-    const getMyChannels = async (value: string) => {
-        await axios.get('/api/chat/channels/me', {
+    // search bar start
+    const [searchChannels, setSearchChannels] = useState<channels[]>([]);
+    const [value, setValue] = useState<string>("");
+    const getSearchChannels = async (value: string) => {
+        await axios.get('/api/chat/channels/all', {
             headers: {
                 'Authorization': 'Bearer ' + jwtToken,
             },
         },)
             .then((response) => {
+                console.log(response.data);
                 const results = response.data.filter((channel: any) => {
                     return (value && channel && channel.name && channel.name.toLowerCase().includes(value.toLowerCase()));
                 })
-                console.log(results);
-                setAllChannels(results);
-                console.log(allChannels);
+                setSearchChannels(results);
             })
             .catch((error) => console.log(error));
     }
 
     const handleValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value,);
-        getMyChannels(event.target.value);
+        getSearchChannels(event.target.value);
     }
+
+    const [password, setPassword] = useState<string>("");
+    const joinChannel = () => {
+        // recuperer le channel qui correspond a value:
+        let channelSelected: channels = { name: "", access: "" };
+        const res = searchChannels.find((item) => item.name === value);
+        if (res) {
+            channelSelected = { name: res.name, access: res.access };
+        }
+        else {
+            channelSelected = { name: value, access: "" }
+        }
+        if (socket) {
+            socket.emit("join", { target: channelSelected.name, password: password })
+        }
+    }
+    // search bar end
 
     useEffect(() => {
         if (socket) {
-            socket.on('create', (data: JSON) => {
-                console.log(data);
-                // fonction ou tu passes le JSON
-                // recharge les channels affiches
+            socket.on('join', (data) => {
+                console.log("socket.on result: " + data.target);
+                console.log(typeof(data.target));
+                if (data && data.target) {
+                    const navigate = useNavigate();
+                    return navigate(`/chat/channel/${data.target}`);
+                }
             });
         }
     }, [socket]);
 
-    //MODALE start {
-    const [isOpen, setIsOpen] = useState(false);
+    const [myChannels, setMyChannels] = useState<string[]>([]);
+    useEffect(() => {
+        const getMyChannels = async () => {
+            const response = await axios.get('/api/chat/channels/me', {
+                headers: {
+                    'Authorization': 'Bearer ' + jwtToken,
+                },
+            },);
+            if (response.status === 200) {
+                if (Array.isArray(response.data)) {
+                    const channels = response.data.map((channel) => channel.name);
+                    setMyChannels(channels);
+                }
+            }
+        }
+        getMyChannels();
+    }, []);
 
-    const openModal = () => {
-        setIsOpen(true);
+    // MODALE Password start
+    const [isOpenPswd, setIsOpenPswd] = useState(false);
+
+    const openModalPswd = () => {
+        setIsOpenPswd(true);
     };
 
-    const closeModal = () => {
+    const closeModalPswd = () => {
         window.location.reload();
-        setIsOpen(false);
+        setIsOpenPswd(false);
     };
-    // } MODALE end
+    // MODALE Password end
+
+    //MODALE NewChannel start {
+    const [isOpenNew, setIsOpenNew] = useState(false);
+
+    const openModalNew = () => {
+        setIsOpenNew(true);
+    };
+
+    const closeModalNew = () => {
+        window.location.reload();
+        setIsOpenNew(false);
+    };
+    // } MODALE NewChannel end
 
     return (
         <div>
@@ -81,37 +135,45 @@ const ChatLayout: React.FC = () => {
                             value={value}
                             onChange={handleValueChange}
                             placeholder="Join a channel" />
-                        <button /*onClick={(renvoie sur la page du channel selectionne)}*/>
-                            <span className="material-symbols-outlined" /*onClick={}*/>add</span>
+                        <button onClick={joinChannel} /*onClick={openModalPswd}*/>
+                            <span className="material-symbols-outlined">add</span>
+                            {/* afficher Modal que si channelSelected.access === protected */}
+                            {/* <Modal
+                                isOpen={isOpenPswd}
+                                onRequestClose={closeModalPswd}
+                                contentLabel="password"
+                                style={customStyles}
+                            >
+                                <button className="material-symbols-outlined" onClick={closeModalPswd}>close</button>
+                            </Modal> */}
                         </button>
                     </div>
-                    <ul>
-                        {
-                            allChannels && allChannels.map((element, index) => <li key={index}>{element.name}</li>)
-                            // value && allChannels && (allChannels.filter((element) => typeof element === 'string' && element.toLowerCase().includes(value.toLowerCase()))
-                            //     .map((element, index) => <li onClick={() => setValue(element)} key={index}>{element}</li>))
-                        }
-                    </ul>
+                    <div className="searchResults">
+                        <ul>
+                            {
+                                searchChannels && searchChannels.map((element, index) => <li onClick={() => setValue(element.name)} key={index}>{element.name}</li>)
+                            }
+                        </ul>
+                    </div>
                 </div>
                 <nav>
                     <ul>
-                        <li>
-                            <NavLink to={`/chat/channel/1`}>Channel1</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/chat/channel/2`}>Channel2</NavLink>
-                        </li>
+                        {myChannels.map((channelName, index) => (
+                            <li key={index}>
+                                <NavLink to={`/chat/channel/${channelName}`}>{channelName}</NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
                 <div id="newChannel">
-                    <button className="button-59" onClick={openModal}>New channel</button>
+                    <button className="button-59" onClick={openModalNew}>New channel</button>
                     <Modal
-                        isOpen={isOpen}
-                        onRequestClose={closeModal}
+                        isOpen={isOpenNew}
+                        onRequestClose={closeModalNew}
                         contentLabel="new channel"
                         style={customStyles}
                     >
-                        <button className="material-symbols-outlined" onClick={closeModal}>close</button>
+                        <button className="material-symbols-outlined" onClick={closeModalNew}>close</button>
                         <CreateChannel />
                     </Modal>
                 </div>
