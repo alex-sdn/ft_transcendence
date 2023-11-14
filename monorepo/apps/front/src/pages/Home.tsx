d4b494fff10c87e019cda5f24b051d4c92ea50e7
alex-sdn <alexsdnwcz@gmail.com>
2023-11-14 14:44:57 +0100

    fix fakelogin button

--- monorepo/apps/front/src/pages/Home.tsx
+++ monorepo/apps/front/src/pages/Home.tsx
@@ -63,10 +63,9 @@ const Home: React.FC = () => {
     return (
         <div>
             {(status === 200) ? <h2>Home</h2> :
-                <h2 className='login'>
-                    <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1b7f717c58b58406ad4b2abe9145475069d66ace504146041932a899c47ff960&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code">Login with 42</a>
-                </h2>
-                <button onClick={handleFakeLogin}>Fake Login</button>
+                <><h2 className='login'>
+					<a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1b7f717c58b58406ad4b2abe9145475069d66ace504146041932a899c47ff960&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code">Login with 42</a>
+				</h2><button onClick={handleFakeLogin}>Fake Login</button></>
             }
         </div>
     );
