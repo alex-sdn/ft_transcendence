d8ad2b8a10702770437eaf660788d00a5e64e5b2
ob-teau <alice.coinus@gmx.fr>
2023-11-14 14:14:24 +0100

    activate 2fa unstaged secret

---
+++ monorepo/apps/front/src/pages/2fa.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import Cookies from "js-cookie";
+import axios from 'axios'
+
+const Twofa: React.FC = () => {
+
+    const jwtToken = Cookies.get('jwt-token');
+    const [stringTwofa, setStringTwofa] = useState<string>('');
+    const [inputCode, setInputCode] = useState<string>('');
+
+    useEffect(() => {
+        const ac2fa = async () => {
+            const response = await axios.post(
+              '/api/user/me/edit2fa', 
+              {}, 
+              {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+              }
+            );
+            if (response.status === 201) {
+              const resp2fa = response.data;
+              setStringTwofa(resp2fa);
+            }
+        };
+        ac2fa(); 
+      }, []);
+  
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputCode(e.target.value);
+      }
+      const handleValidation = () => {
+        ac2fabutton();
+      }
+  
+      const ac2fabutton = async () => {
+  
+          const responsesecret = await axios.post(
+              '/api/user/me/activate2fa', {code: inputCode},
+             {headers : {
+                 'Authorization' : 'Bearer ' + jwtToken,
+             },});
+             console.log('**************' + responsesecret.status);
+             if (responsesecret.status === 200) {
+              console.log('2FA activated successfully'); 
+              window.location.reload();
+             }
+            else {
+            console.log('**************' + responsesecret.status);
+              console.log('2fa FAILED')}}
+    return (
+        <div>
+        <img src={stringTwofa} alt="QR code" />
+        <input type='text' name='code' id='code' value={inputCode} onChange={handleChange} placeholder='Enter OTP'/>
+        <button className='button-29' onClick={handleValidation}>ok</button>
+        </div>
+    )
+}
+
+export default Twofa;
