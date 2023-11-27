078898fd3980c0239895154d919286c531262a52
Solene <aguesse.solene2@neuf.fr>
2023-11-27 11:27:15 +0100

    test socket failed

--- monorepo/apps/front/src/Socket.tsx
+++ monorepo/apps/front/src/Socket.tsx
@@ -2,19 +2,35 @@ import React, { useEffect } from 'react';
 import { io, Socket } from 'socket.io-client';
 import Cookies from "js-cookie";
 
-const SocketContext = React.createContext<Socket | null>(null);
+// const SocketContext = React.createContext<Socket | null>(null);
 
-export default SocketContext;
+// export default SocketContext;
 
-export function initializeSocket(): Socket | undefined {
-    const jwtToken = Cookies.get('jwt-token');
-    if (!jwtToken)
-        return;
-    const socket = io("http://localhost:3000/chat1", {
-    // const socket = io("http://made-f0br7s13:3000/chat1", {
-        extraHeaders: {
-            'Authorization': 'Bearer ' + jwtToken,
-        }
-    });
-    return socket;
-}
+// export function initializeSocket(): Socket | undefined {
+//     const jwtToken = Cookies.get('jwt-token');
+//     if (!jwtToken) {
+//         // window.location.assign('/login');
+//         return;
+//     }
+//     const socket = io("http://localhost:3000/chat1", {
+//         extraHeaders: {
+//             'Authorization': 'Bearer ' + jwtToken,
+//         }
+//     });
+// return socket;
+// }
+
+export type SocketContextType = Socket;
+
+const socket = io("http://localhost:3000/chat1");
+export const SocketContext = React.createContext(socket);
+
+const SocketProvider = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export default SocketProvider;
