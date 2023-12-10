8f7a09de31ebbb20c6fdf26baf63e49d3bd010c8
alice <alice.coinus@gmx.fr>
2023-12-10 23:09:16 +0100

    profile nearly done (achievement)

--- monorepo/apps/front/src/pages/ProfileUser.tsx
+++ monorepo/apps/front/src/pages/ProfileUser.tsx
@@ -18,7 +18,6 @@ const ProfileUser: React.FC = () => {
   const [id, setId] = useState<number>();
   const [matches, setMatches] = useState<any[]>([]);
   const [isFriend, SetIsFriend] = useState<boolean>(false);
-  const [isadded, SetIsAdded] = useState<boolean>(false);
   const [isBlocked, setIsBlocked] = useState<boolean>(false);
   const [blockModal, setBlockModal] = useState<boolean>(false);
   const [showPopup, setShowPopup] = useState(false);
@@ -26,104 +25,118 @@ const ProfileUser: React.FC = () => {
 
   // REQUETE INFOS
   useEffect(() => {
-    const getProfileData = async () => {
-      const response = await axios.get(`/api/user/id/${ID}`, {
-        headers: {
-          'Authorization': 'Bearer ' + jwtToken,
-        },
-      },);
-      if (response.status === 200) {
-        const resp_profile = response.data;
-        setNickname(resp_profile.nickname);
-        setLoss(resp_profile.loss);
-        setWin(resp_profile.win);
-        setLp(resp_profile.LP);
-        setGameNb(resp_profile.loss + resp_profile.win);
-        setId(resp_profile.id);
+      const getProfileData = async () => {
+          try {
+              const response = await axios.get(`/api/user/id/${ID}`, {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+              },);
+              if (response.status === 200) {
+                const resp_profile = response.data;
+                setNickname(resp_profile.nickname);
+                setLoss(resp_profile.loss);
+                setWin(resp_profile.win);
+                setLp(resp_profile.LP);
+                setGameNb(resp_profile.loss + resp_profile.win);
+                setId(resp_profile.id);
+              }
+          }
+          catch (error)
+              {console.log(error);}
       }
-    }
-    getProfileData();
+      getProfileData();
   }, [id]);
 
   //REQUETE MATCHS
   useEffect(() => {
-    const getMatches = async () => {
-      console.log('token = ', jwtToken);
-      const response = await axios.get(`/api/user/matches/${ID}`, {
-        headers: {
-          'Authorization': 'Bearer ' + jwtToken,
-        },
-      },);
-      if (response.status === 200) {
-        const resp_match = response.data;
-        setMatches(resp_match);
-        console.log(resp_match);
+      const getMatches = async () => {
+          try {
+              const response = await axios.get(`/api/user/matches/${ID}`, {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+              },);
+              if (response.status === 200) {
+                const resp_match = response.data;
+                setMatches(resp_match);
+                console.log(resp_match);
+              }
+          }
+          catch (error)
+              {console.log(error);}
       }
-    }
-    getMatches();
+      getMatches();
   }, [id]);
 
 //REQUETE AVATAR
   useEffect(() => {
-    const fetchDefaultAvatar = async () => {
-      let response = await axios.get(`/api/user/id/${ID}`, {
-        headers: {
-          'Authorization': 'Bearer ' + jwtToken,
-        },
-      },);
-      const fileName = response.data.avatar;
-      response = await axios.get('/api/user/avatar/' + fileName, {
-        headers: {
-          'Authorization': 'Bearer ' + jwtToken,
-        },
-        responseType: 'arraybuffer',
-      }, );
-      if (response.status === 200) {
-        const blob = new Blob([response.data]);
-
-        const file = new File([blob], fileName);
-        setImage(file);
-      }
-    };
-    fetchDefaultAvatar();
+      const fetchDefaultAvatar = async () => {
+          try{
+              let response = await axios.get(`/api/user/id/${ID}`, {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+              },);
+              const fileName = response.data.avatar;
+              response = await axios.get('/api/user/avatar/' + fileName, {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+                responseType: 'arraybuffer',
+              }, );
+              if (response.status === 200) {
+                const blob = new Blob([response.data]);
+              
+                const file = new File([blob], fileName);
+                setImage(file);
+              }
+          }
+          catch (error)
+              {console.log(error);}
+      };
+      fetchDefaultAvatar();
   }, [id]);
 
   //BLOCK
   useEffect(() => {
       const getBlocked = async () => {
-          const response = await axios.get(`/api/user/block/${nickname}`, {
-              headers: {
-                  'Authorization': 'Bearer ' + jwtToken,
-              },
-          })
-          if (response.status === 200) {
-              setIsBlocked(response.data);
+          try {
+            const response = await axios.get(`/api/user/block/${nickname}`, {
+                headers: {
+                    'Authorization': 'Bearer ' + jwtToken,
+                },
+            })
+            if (response.status === 200) 
+                {setIsBlocked(response.data);}
           }
+          catch (error)
+              {console.log(error);}
       }
       getBlocked()
   }, [nickname, jwtToken])
   
 //GET FRIEND
 useEffect(() => {
-  const getFriend = async () => {
-    console.log('token = ', jwtToken);
-    const response = await axios.get(`/api/user/friend/${ID}`, {
-      headers: {
-        'Authorization': 'Bearer ' + jwtToken,
-      },
-    },);
-    if (response.status === 200) {
-      SetIsFriend(response.data)
+    const getFriend = async () => {
+        try { 
+            const response = await axios.get(`/api/user/friend/${ID}`, {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+            },);
+            if (response.status === 200) 
+            {SetIsFriend(response.data)}
+        }
+        catch (error)
+            {console.log(error);}
     }
-    else 
-    {console.log("Error./ get /api/user/friend/${ID}");}
-  }
-  getFriend();
+    getFriend();
 },);
 
 //POST FRIEND
 const postClic = () => {
-  postFriend();
+    postFriend();
 }
 
 const postFriend = async () => {
@@ -137,40 +150,37 @@ const postFriend = async () => {
       },
     }
   );
-  console.log("response status = ");
-  console.log(response.status);
   if (response.status === 201) 
-  { console.log("request sent");
-   window.location.reload();}
+      {window.location.reload();}
   }
   catch (error)
   { 
     setShowPopup(true);
-    console.log("Error./ post /api/user/friend/${ID}");}
+    console.log(error);}
 };
 
 setTimeout(() => {
-  setShowPopup(false);
+    setShowPopup(false);
 }, 4000);
 
 //DELETE FRIEND
 const dltClic = () => {
-  dltFriend();
+    dltFriend();
 }
 
-  const dltFriend = async () => {
-    console.log('token = ', jwtToken);
-    const response = await axios.delete(`/api/user/friend/${ID}`, {
-      headers: {
-        'Authorization': 'Bearer ' + jwtToken,
-      },
-    },);
-    if (response.status === 200) 
-    {  console.log("friend deleted");
-        window.location.reload();}
-    else 
-    {console.log("Error./ delete /api/user/friend/${ID}");}
-  }
+const dltFriend = async () => {
+    try {
+        const response = await axios.delete(`/api/user/friend/${ID}`, {
+            headers: {
+              'Authorization': 'Bearer ' + jwtToken,
+            },
+        },);
+        if (response.status === 200) 
+            {window.location.reload();}
+    }
+    catch (error)
+        {console.log(error);}
+}
 
 
   return (
@@ -216,47 +226,24 @@ const dltClic = () => {
     </div>
     <div className='_scoreTab'>
         <h1>Recent Games</h1>
-        {matches.length > 0 && (
+        {matches.length > 0 ?
             <ul>
                 {matches.map((match, index) =>
-                (
-                    <ul key={index + 1}>
-                    {id === match.user1.id ? 
-
-                    // ecrire si victory ou defeat
                     <div>
-                    {/* <div>ID = {id}&nbsp;&nbsp;MATCH USER1 ID = {match.user1.id}</div> */}
                     {match.p1score < match.p2score ? <span className='_defeat'> {match.p1score} / {match.p2score} : &nbsp;DEFEAT</span> : <span className='_victory'>  {match.p1score} / {match.p2score} : &nbsp; VICTORY</span>}
-                    {/* ecrire against qui ? */}
                     &nbsp;against&nbsp;<span className='_nickname'> {match.user2.nickname}</span>
-                    {/* ecrire la date */}
                     &nbsp;<span className='_date'> {new Date(match.date).toLocaleDateString(undefined, {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric',
                     })} </span> 
-
+                
                     </div>
-
-                      : 
-                  
-                    // ecrire si victory ou defeat
-                    <div>
-                    
-                    {match.p1score > match.p2score ? <span className='_defeat'>{match.p2score} / {match.p1score} &nbsp; DEFEAT</span> : <span className='_victory'>{match.p2score} / {match.p1score} &nbsp; VICTORY</span>}
-                    {/* ecrire against qui ? */}
-                    &nbsp; against&nbsp; <span className='_whiteTab'>{match.user1.nickname} </span>
-                    {/* ecrire la date */}
-                    &nbsp;On&nbsp; <span className='_blackTab'>{match.date} </span>
-                  
-                    </div>
-
-                    }
-                    </ul>
-                )
                 )}
             </ul>
-        )}
+            :
+            <div> No matches done yet ! 🤺 </div>
+        }
     </div>
 </div>
       )
--- monorepo/apps/front/src/pages/Profilefriends.tsx
+++ monorepo/apps/front/src/pages/Profilefriends.tsx
@@ -1,63 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from "js-cookie";
 import axios from 'axios'
-// import Modal from 'react-modal';
-// import SearchNick from './SearchNick';
 
 const ProfileFriends: React.FC = () => {
-    const jwtToken = Cookies.get('jwt-token');
-    const [requests, setRequests] = useState<any[]>([]);
+  const jwtToken = Cookies.get('jwt-token');
+  const [requests, setRequests] = useState<any[]>([]);
 
-    useEffect(() => {
-        const friendsreq = async () => {
-          console.log('token = ', jwtToken);
-          const response = await axios.get('/api/user/me/friend/requests', {
+  useEffect(() => {
+      const friendsreq = async () => {
+        try {
+            const response = await axios.get('/api/user/me/friend/requests', {
+              headers: {
+                'Authorization': 'Bearer ' + jwtToken,
+              },
+            },);
+            if (response.status === 200) {
+              const resp_req = response.data;
+              setRequests(resp_req);
+              console.log(resp_req);
+            }
+        }
+        catch (error)
+        {console.log(error);}
+      }
+      friendsreq();
+  },[]);
+
+//ADD    
+  const postFriend = async (ID : string) => {
+      try {
+          const response = await axios.post(
+              `/api/user/friend/${ID}`, 
+              {}, 
+              {
+                headers: {
+                  'Authorization': 'Bearer ' + jwtToken,
+                },
+              }
+          );
+          if (response.status === 201) 
+              {return window.location.reload();}
+      }
+      catch (error)
+          {console.log(error);}
+  };
+
+//REFUSE
+  const dltFriend = async (ID :string) => {
+      try{
+          const response = await axios.delete(`/api/user/friend/${ID}`, {
             headers: {
               'Authorization': 'Bearer ' + jwtToken,
             },
           },);
-          if (response.status === 200) {
-            const resp_req = response.data;
-            setRequests(resp_req);
-            console.log(resp_req);
-          }
-          else 
-          {}
-        }
-        friendsreq();
-      },[]);
-
-//ADD    
-      const postFriend = async (ID : string) => {
-        try {
-        const response = await axios.post(
-          `/api/user/friend/${ID}`, 
-          {}, 
-          {
-            headers: {
-              'Authorization': 'Bearer ' + jwtToken,
-            },
-          }
-        );
-        if (response.status === 201) 
-        {    return window.location.reload();}
-        }
-        catch (error)
-        {}
-      };
-
-//REFUSE
-const dltFriend = async (ID :string) => {
-    console.log('token = ', jwtToken);
-    const response = await axios.delete(`/api/user/friend/${ID}`, {
-      headers: {
-        'Authorization': 'Bearer ' + jwtToken,
-      },
-    },);
-    if (response.status === 200) 
-    {    return window.location.reload();}
-    else 
-    {console.log("Error./ delete /api/user/friend/${ID}");}
+          if (response.status === 200) 
+          {    return window.location.reload();}
+      }
+      catch (error)
+          {console.log(error);}
   }
 
 //MODALE
@@ -89,24 +89,7 @@ const closeModalfa = () => {
                         </ul>
           :
                 <p className='_score'><br/>No Pending Requests <br/></p>
-          }
-
-            {/* {requests.length > 0 &&  (
-                <ul>
-                    {requests.map((request, index) =>
-                    (
-                        <ul key={index + 1}>
-                                <div>
-                                    <button className="_victory" onClick={() => postFriend(request.requester.id)}>✅</button>
-                                    <button className="_defeat" onClick={() => dltFriend(request.requester.id)}>❌</button>
-                                    <span className='_score'>{request.requester.nickname}</span>
-                                </div>
-                        </ul>
-                    ))}
-                </ul>
-            )
-            } */}
-            
+          }            
         </div>
         )
       }
