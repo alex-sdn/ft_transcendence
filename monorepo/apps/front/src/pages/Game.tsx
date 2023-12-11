642ef039f0821a04b8c1d9153776bc5ab34682a6
smk <sacha.mkara@gmail.com>
2023-12-11 23:50:42 +0100

    cool cat playable but not as i expected

--- monorepo/apps/front/src/pages/Game.tsx
+++ monorepo/apps/front/src/pages/Game.tsx
@@ -270,15 +270,18 @@ const Game: React.FC = () => {
         const handleKeyPress = (event: any) => {
             if (event.type === 'keydown') {
                 if (event.key === 'ArrowUp') {
-                    socket?.emit('keys', { action: 'upPressed', roomName: roomName, role: role });
+                    if (!Coolcat)
+                        socket?.emit('keys', { action: 'upPressed', roomName: roomName, role: role });
                 }
                 if (event.key === 'ArrowDown') {
-                    socket?.emit('keys', { action: 'downPressed', roomName: roomName, role: role });
+                    if (!Coolcat)
+                        socket?.emit('keys', { action: 'downPressed', roomName: roomName, role: role });
                 }
             }
             else if (event.type === 'keyup') {
                 if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-                    socket?.emit('keys', { action: 'released', roomName: roomName, role: role });
+                    if (!Coolcat)
+                        socket?.emit('keys', { action: 'released', roomName: roomName, role: role });
                 }
             }
         };
@@ -289,6 +292,7 @@ const Game: React.FC = () => {
             window.removeEventListener('keyup', handleKeyPress);
         };
     }, [roomName]);
+
 
     /******************************************************************************
     *                               GAME OPTIONS                                  *
@@ -538,13 +542,30 @@ const Game: React.FC = () => {
         }
     }, [puckPos]);
 
+
+    const canvasX = (window.innerWidth - gameConst.PLAYGROUND_WIDTH / 4) / 2;
+    const canvasY = (window.innerHeight - gameConst.PLAYGROUND_HEIGHT / 4) / 2;
+
+    let fireworks: any[] = [];
+
     function setup(p5: p5Types) {
-        p5.createCanvas(gameConst.PLAYGROUND_WIDTH / 2, gameConst.PLAYGROUND_HEIGHT / 2);
+
+        p5.createCanvas(gameConst.PLAYGROUND_WIDTH / 4, gameConst.PLAYGROUND_HEIGHT / 4).position(canvasX, canvasY);
+        for (let i = 0; i < 10; i++) {
+            fireworks.push({
+                x: p5.random(gameConst.PLAYGROUND_WIDTH / 4),
+                y: p5.random(gameConst.PLAYGROUND_HEIGHT / 4),
+                angle: p5.random(p5.TWO_PI),
+                speed: p5.random(1, 3),
+                colors: ['#FF0000', '#FFA500', '#FFFF00', '#00FF00', '#0000FF', '#800080'],
+            });
+        }
     }
 
-    function drawStar(p5: p5Types, x: any, y: any, radius1: any, radius2: any, npoints: any) {
+    function drawStar(p5: p5Types, x: any, y: any, radius1: any, radius2: any, npoints: any, color: any) {
         let angle = p5.TWO_PI / npoints;
         let halfAngle = angle / 2.0;
+        p5.fill(color);
         p5.beginShape();
         for (let a = 0; a < p5.TWO_PI; a += angle) {
             let sx = x + p5.cos(a) * radius2;
@@ -557,39 +578,81 @@ const Game: React.FC = () => {
         p5.endShape(p5.CLOSE);
     }
 
+    function drawFirework(p5: p5Types, firework: any) {
+        for (let i = 0; i < 6; i++) {
+            const angle = firework.angle + i * (p5.TWO_PI / 6);
+            const px = firework.x + 20 * p5.cos(angle);
+            const py = firework.y + 20 * p5.sin(angle);
+
+            p5.fill(firework.colors[i]);
+            p5.ellipse(px, py, 10, 10);
+        }
+    }
+
+    function updateFirework(p5: p5Types, firework: any) {
+        firework.y += firework.speed;
+
+        // Reset the firework if it goes off the screen
+        if (firework.y > gameConst.PLAYGROUND_HEIGHT / 4) {
+            firework.x = p5.random(gameConst.PLAYGROUND_WIDTH / 4);
+            firework.y = p5.random(-50, 0);
+            firework.angle = p5.random(p5.TWO_PI);
+            firework.speed = p5.random(1, 3);
+        }
+    }
+
     function draw(p5: p5Types) {
 
         p5.background(255);
         p5.noStroke();
 
-        // traînée filante
+        p5.fill(0);
+        p5.textSize(24);
+        p5.textAlign(p5.CENTER, p5.TOP);
+        p5.text(`${score.left} - ${score.right}`, gameConst.PLAYGROUND_WIDTH / 8, 10);
+
+        p5.textFont('Orbitron');
+        p5.textSize(16);
+        p5.textAlign(p5.CENTER, p5.BOTTOM);
+        p5.text(`${nickname.left} - ${nickname.right}`, gameConst.PLAYGROUND_WIDTH / 8, gameConst.PLAYGROUND_HEIGHT / 4 - 10);
+
+        let gradientColor: any;
+
+        // puck or star
         if (score.left >= 2 || score.right >= 2) {
+            gradientColor = p5.lerpColor(p5.color(255, 0, 0), p5.color(0, 0, 255), puckPos.x / gameConst.PLAYGROUND_WIDTH);
+            if (score.left >= 3 || score.right >= 3)
+                drawStar(p5, puckPos.x / 4, puckPos.y / 4, 5, 10, 5, gradientColor);
+            else
+                drawStar(p5, puckPos.x / 4, puckPos.y / 4, 5, 10, 5, p5.color(0, 0, 0));
+        }
+        else {
+            p5.ellipse(puckPos.x / 4, puckPos.y / 4, 10, 10);
+        }
+
+        // traînée filante
+        if (score.left >= 4 || score.right >= 4) {
             previousPuckPositions.forEach((position, index) => {
                 const alphaValue = p5.map(10 - index, 0, previousPuckPositions.length - 1, 255, 0);
-                p5.fill(0, alphaValue);
-                p5.ellipse(position.x / 2, position.y / 2, 20 - index, 20 - index);
+                p5.fill(gradientColor.levels[0], gradientColor.levels[1], gradientColor.levels[2], alphaValue);
+                p5.ellipse(position.x / 4, position.y / 4, 20 - index, 20 - index);
             });
         }
 
-        // puck
-        if (score.left >= 1 || score.right >= 1) {
-            drawStar(p5, puckPos.x / 2, puckPos.y / 2, 10, 20, 5);
-        }
-        else {
-            p5.ellipse(puckPos.x / 2, puckPos.y / 2, 20, 20);
-        }
         // paddles
-        if (score.left >= 6 || score.right >= 6)
-            p5.fill("blue");
-        else
-            p5.fill("black");
-        p5.rect(gameConst.PADDLE_OFFSET / 2, paddle.leftPos / 2 - gameConst.PADDLE_HEIGHT / 4, gameConst.PADDLE_WIDTH / 2, gameConst.PADDLE_HEIGHT / 2);
+        p5.fill("black");
+        p5.rect(gameConst.PADDLE_OFFSET / 4, paddle.leftPos / 4 - gameConst.PADDLE_HEIGHT / 8, gameConst.PADDLE_WIDTH / 4, gameConst.PADDLE_HEIGHT / 4);
         p5.rect(
-            gameConst.PLAYGROUND_WIDTH / 2 - gameConst.PADDLE_WIDTH / 2 - gameConst.PADDLE_OFFSET / 2,
-            paddle.rightPos / 2 - gameConst.PADDLE_HEIGHT / 4,
-            gameConst.PADDLE_WIDTH / 2,
-            gameConst.PADDLE_HEIGHT / 2
+            gameConst.PLAYGROUND_WIDTH / 4 - gameConst.PADDLE_WIDTH / 4 - gameConst.PADDLE_OFFSET / 4,
+            paddle.rightPos / 4 - gameConst.PADDLE_HEIGHT / 8,
+            gameConst.PADDLE_WIDTH / 4,
+            gameConst.PADDLE_HEIGHT / 4
         );
+
+        for (let i = 0; i < fireworks.length; i++) {
+            drawFirework(p5, fireworks[i]);
+            updateFirework(p5, fireworks[i]);
+        }
 
         if (p5.keyIsPressed) {
             if (p5.keyCode == p5.UP_ARROW) {
@@ -604,7 +667,9 @@ const Game: React.FC = () => {
         <div>
             <div>
                 {Coolcat && !AskOption && !AskReady && !ScreenIssue && !Countdown && (Count <= 0) &&
-                    <Sketch setup={setup} draw={draw} />
+                    <div className="sketch-container">
+                        <Sketch setup={setup} draw={draw} />
+                    </div>
                 }
                 {AskOption && (
                     <div className="button-container">
