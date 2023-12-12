df655e7c2242054efff98f1273388831c4b4f7a4
Solene <aguesse.solene2@neuf.fr>
2023-12-12 11:41:15 +0100

    error messages 2fa

--- monorepo/apps/front/src/pages/2fa.tsx
+++ monorepo/apps/front/src/pages/2fa.tsx
@@ -4,65 +4,75 @@ import axios from 'axios'
 
 const Twofa: React.FC = () => {
 
-    const jwtToken = Cookies.get('jwt-token');
-    const [stringTwofa, setStringTwofa] = useState<string>('');
-    const [inputCode, setInputCode] = useState<string>('');
+  const jwtToken = Cookies.get('jwt-token');
+  const [stringTwofa, setStringTwofa] = useState<string>('');
+  const [inputCode, setInputCode] = useState<string>('');
+  const [error, setError] = useState<string>("");
 
-    useEffect(() => {
-        const ac2fa = async () => {
-          try{
-              const response = await axios.post(
-                '/api/user/me/edit2fa', 
-                {}, 
-                {
-                  headers: {
-                    'Authorization': 'Bearer ' + jwtToken,
-                  },
-                }
-              );
-              if (response.status === 201) {
-                const resp2fa = response.data;
-                setStringTwofa(resp2fa);
-              }
+  useEffect(() => {
+    const ac2fa = async () => {
+      try {
+        const response = await axios.post(
+          '/api/user/me/edit2fa',
+          {},
+          {
+            headers: {
+              'Authorization': 'Bearer ' + jwtToken,
+            },
           }
-          catch (error)
-              {console.log(error);}
-        };
-        ac2fa(); 
-      }, []);
-  
-
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputCode(e.target.value);
+        );
+        if (response.status === 201) {
+          const resp2fa = response.data;
+          setStringTwofa(resp2fa);
+        }
       }
-      const handleValidation = () => {
-        ac2fabutton();
-      }
+      catch (error) { console.log(error); }
+    };
+    ac2fa();
+  }, []);
 
-      const ac2fabutton = async () => {
-          try {
-              const responsesecret = await axios.post(
-                  '/api/user/me/activate2fa', {code: inputCode},
-                 {headers : {
-                     'Authorization' : 'Bearer ' + jwtToken,
-                     'Content-Type': 'application/json',
-                 },});
-                 if (responsesecret.status === 201) {
-                  console.log('\n ******** 2FA post successful');
-                  window.location.reload();
-                 }
-            }
-            catch (error) 
-                {console.log(error);}
-      }
 
-    return (
-        <div>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputCode(e.target.value);
+    setError("");
+  }
+  const handleValidation = () => {
+    ac2fabutton();
+  }
+
+  const ac2fabutton = async () => {
+    try {
+      const responsesecret = await axios.post(
+        '/api/user/me/activate2fa', { code: inputCode },
+        {
+          headers: {
+            'Authorization': 'Bearer ' + jwtToken,
+            'Content-Type': 'application/json',
+          },
+        });
+      if (responsesecret.status === 201) {
+        setError("");
+        window.location.reload();
+      }
+    }
+    catch (error) {
+      console.log(error.response.data.message);
+      setError(error.response.data.message);
+    }
+  }
+
+  return (
+    <div>
+      <p className="twofa-center">
         <img src={stringTwofa} alt="QR code" />
-        <input type='text' name='code' id='code' value={inputCode} onChange={handleChange} placeholder='Enter OTP'/>
+      </p>
+      <p className="twofa-center">
+        <input type='text' name='code' id='code' value={inputCode} onChange={handleChange} placeholder='Enter OTP' />
         <button className='button-29' onClick={handleValidation}>ok</button>
-        </div>
-    )
+      </p>
+      {error && <div className="text-danger twofa-center">{error}</div>}
+    </div>
+  )
 }
 
 export default Twofa;
