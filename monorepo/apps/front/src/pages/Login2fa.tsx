dee28b32d07d3918f9df9e34d9bb2776eb189d83
Solene <aguesse.solene2@neuf.fr>
2023-12-12 11:41:15 +0100

    error messages 2fa

--- monorepo/apps/front/src/pages/Login2fa.tsx
+++ monorepo/apps/front/src/pages/Login2fa.tsx
@@ -6,42 +6,52 @@ const Login2fa: React.FC = () => {
 
   const jwtToken = Cookies.get('jwt-2fa-token');
   const [inputCode, setInputCode] = useState<string>('');
+  const [error, setError] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputCode(e.target.value);
+    setError("");
   }
   const handleValidation = () => {
     login2fareq(inputCode);
   }
 
   const login2fareq = async (code: string) => {
-      try {
-          const responsesecret = await axios.post('/api/auth/signin/2fa', { code: code },
-            {
-              headers: {
-                'Authorization': 'Bearer ' + jwtToken,
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-            });
-          
-          if (responsesecret.status === 201) {
-            Cookies.remove('jwt-2fa-token');
-            Cookies.set("jwt-token", responsesecret.data, { expires: 1 });
-            window.location.assign('/');
-            return;
-          }
+    try {
+      const responsesecret = await axios.post('/api/auth/signin/2fa', { code: code },
+        {
+          headers: {
+            'Authorization': 'Bearer ' + jwtToken,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+
+      if (responsesecret.status === 201) {
+        setError("");
+        Cookies.remove('jwt-2fa-token');
+        Cookies.set("jwt-token", responsesecret.data, { expires: 1 });
+        window.location.assign('/');
+        return;
       }
-      catch (error) 
-          {console.log(error);}
+    }
+    catch (error) {
+      console.log(error.response.data.message);
+      setError(error.response.data.message);
+    }
   }
 
 
   return (
 
-    <div className='_info' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div>OTP Needed from your authentifier</div><br />
-      <input type='text' name='code' id='code' value={inputCode} onChange={handleChange} placeholder='Enter OTP' />
-      <button className='button-29' onClick={handleValidation}>ok</button>
+    <div className='_info'>
+      <p className="twofa-center">
+        OTP Needed from your authentifier
+      </p>
+      <p className="twofa-center">
+        <input type='text' name='code' id='code' value={inputCode} onChange={handleChange} placeholder='Enter OTP' />
+        <button className='button-29' onClick={handleValidation}>ok</button>
+      </p>
+      {error && <div className="text-danger twofa-center">{error}</div>}
     </div>
   )
 }
